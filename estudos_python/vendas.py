1763aff3727cb7fda298e951923d3eff28198be1
roberto <robertoweller78@gmail.com>
2021-03-18 05:49:20 -0300

    Corrigido e deixado meio caminho andado

--- estudos_python/vendas.py
+++ estudos_python/vendas.py
@@ -1,23 +1,22 @@
-from abc import abstractproperty
-
 
 def processar():
+    """
+    Função criada para processar pedido do usuario e disignar
+    para suas devidas ações
+    """
     
     try:
-
-        print('Processando programa...')
 
         # O usuario digitou
         usuario = input('Digite! ')
 
         # Listas de funções
-        lista = [chama, chamouN, ira]
+        lista = [cadastrar, vender, exibir]
 
         # Se usuario digitar 0 ou o que digitar for maior q a lista, 
-        # não vai chamar a função, isso para evitar bug
-        
+        # não vai chamar a função, isso foi feito para evitar bug
         if int(usuario) <= len(lista) and usuario not in '0':
-        # Vai procurar dentro da lista qual é a função para chamar e efetua!
+        # Vai procurar dentro da lista qual é a função para chamar e chama ela!
             lista[int(usuario)-1]()
 
         return usuario
@@ -26,33 +25,50 @@ def processar():
     # e continuar rodando normalmente
     except ValueError:
         # Sla, resolver aqui
-        print('Erro 404!')
+        print('Opção não válida! ')
         # processar()
     
 
-def chama():
-    print('Chamou???')
+def cadastrar():
+    """
+    Precisa resolver como vai efeturar cadastro
+    """
+    nome = input('Nome: ')
+    quantidade = input('Quantidade: ')
+    valor_unidade = input('Valor Unitário: ')
+    print('')
+
     pass
 
-def chamouN():
-    print('Não chamou?')
+def vender():
+    """
+    Precisa resolver como os desconto do produto na lista que será feito
+    """
+    id = input('Código do produto: ')
+    # Talvez deva escolher outra variavel para facilitar
+    quantidade = input('Quantidade: ')
+    # Talve nem vá precisar, só se tiver desconto.
+    valor_total = input('Valor total da compra: ')
     pass
 
-def ira():
-    print('irar chamarrrr!')
+def exibir():
+    """
+    Provavelmente terá q ser feito um for nos cadastro produtos q vc criou
+    """
+    print('Exibira quantos produtos tem!\n')
     pass
 
 
 usu = True
 
+# Quando usuario digitar 0 vai parar o luping e sair do programa
 while usu != '0':
-        
+    print(
+        f'-----------------------------------------------'
+        '--------------------------\n'
+        'Sistema de vendas online:',
+        '1-Cadastrar, 2-Vender, '
+        '3-Exibir, 0-Finalizar: ')
     # print(f"{usu=}")
     # Vai processar e aderir a variavel
     usu = processar()
-
-
-
-
-    
-
