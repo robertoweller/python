d28df534d1621e24b331a168d857d21d4a2426eb
roberto <robertoweller78@gmail.com>
2021-03-18 19:03:43 -0300

    Resolvido exercicio

--- estudos_python/dna.py
+++ estudos_python/dna.py
@@ -1,10 +1,57 @@
 
-print('DNA contem A, C, G, e T, acho kk')
+print('Nesse exemplo contem A, C, G, e T \n')
+
+
+
+"""
+No exemplo:
+
+AATCTGCAC virou TTAGACGTG
+
+TATGCGCAC virou ATACGCGTG
+
+
+Pesquisando na internet sobre DNA é tipo assim que funciona:
+
+Olhando para baixo da para começar enteder
+
+A - T
+T - A
+T - A
+G - C
+C - G
+A - T
+T - A
+G - C
+C - G
+G - C
+C - G
+A - T
+T - A
+T - A
+A - T
+C - G
+G - C
+
+
+"""
+
+# Mas o exercio não pede uma estrutura tão complexa igual então
+# Esse dicionario resolve
+complementar = {
+    'A':'T',
+    'T':'A',
+    'C':'G',
+    'G':'C',
+}
+
 
 # O dna é A, C, G, e T
 dn = ['A', 'C', 'G', 'T']
 
-dna = input('Digite o dna: ')
+dna = input('Digite uma sequência de DNA: ')
+
+corrigido = []
 
 manipular = list()
 
@@ -15,11 +62,18 @@ for d in dna:
     # print(d)
     # Para começar entender vou usar if, mas recomendavel usar um dicionario
     if d in dn:
-        manipular.append(d)
+        corrigido.append(d)
+        # print(f'{d=}')
+        # Vai no dicionario e muda a letra
+        # e adiciona a lista 
+        manipular.append(complementar[d])
+
+
 
 # manipular vai ser que será usada
 # juntar vai deixar de ser uma lista para virar uma string
 junta = ''.join(manipular)
+corrigido = ''.join(corrigido)
 
 # junta vai ser o DNA, não sei de DNA, mas acho que é o q pede no exercicio
-print(f'\n {junta=}')
+print(f'\n A cadeia complementar de {corrigido} é {junta}')
