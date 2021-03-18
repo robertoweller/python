809953b312fc2c2957c1b45e8c8a7a3b6ca5766d
roberto <robertoweller78@gmail.com>
2021-03-18 17:13:25 -0300

    Acho que da para ter uma forma

---
+++ estudos_python/dna.py
@@ -0,0 +1,24 @@
+
+print('DNA contem A, C, G, e T, acho kk')
+
+# O dna é A, C, G, e T
+dn = ['A', 'C' 'G', 'T']
+
+dna = input('Digite o dna: ')
+
+manipular = list()
+
+for d in dna:
+    
+    # Vai deixar a string em letra maiuscula
+    d = d.capitalize()
+    # print(d)
+    # Para começar entender vou usar if, mas recomendavel usar um dicionario
+    if d in dn:
+        manipular.append(d)
+
+# manipular vai ser que será usada
+# juntar vai deixar de ser uma lista para virar uma string
+junta = ', '.join(manipular)
+
+print(f'\n {junta=}')
