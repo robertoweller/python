187737915c39857bc5863ba734c34bed4733b809
roberto <robertoweller78@gmail.com>
2021-03-21 22:50:15 -0300

    Pegando ultimo elemento

--- estudos_python/lista.py
+++ estudos_python/lista.py
@@ -1,7 +1,41 @@
-nome_s = input('Digite nome inteiro do autor! ')
 
-nome_s = nome_s.split()
+'''
 
-nome_s = f'{nome_s[1]} {nome_s[0]}'
+a = input('Digite o titulo do livro:')
+b = input('Digite os autores do livro:')
+c = input('Ano de publicação do livro:')
+d = input('Cidade:')
+e = input('Editora do livro:')
 
-print(f'{nome_s}')
+
+
+'''
+
+# Digamos que essa sejaa a resposta
+a = 'Perditubes'
+b = 'Roberto Weller, Jao Solza'
+c= '1996'
+d= 'SFS'
+e = 'EdMensoes'
+
+# ??? Erro
+autor_1, autor_2 = b.split(',')
+
+nome, sobrenome = autor_1.split()
+nome_2, sobrenome_2 = autor_2.split()
+
+nome_com = [sobrenome, nome, sobrenome_2, nome_2]
+
+
+# Desempacotar todos os nomes juntos
+nom_lis = ' '.join(nome_com)
+
+# A variavel b contém nome
+
+
+resulado = f'{nom_lis}, {a}. {d}: {e}, {c}'
+
+
+
+# Retorna uma tupla
+print(resulado)
