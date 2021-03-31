07715afeb6b30a196edb7b7aa0e502a632e78130
roberto <robertoweller78@gmail.com>
2021-03-31 18:28:09 -0300

    Jogado como vai funciona par auma função

--- estudos_python/cadastro_musica/musica.py
+++ estudos_python/cadastro_musica/musica.py
@@ -1,45 +1,46 @@
-# Variaveis principais
-nomes = []
-cantores = []
-n_tocadas = []
-letras = {}
-opcao = -1
+def ideia():
+    # Variaveis principais
+    nomes = []
+    cantores = []
+    n_tocadas = []
+    letras = {}
+    opcao = -1
 
-print("---ProgFy: O melhor streaming de música---")
-print("---Cadastro de músicas---")
+    print("---ProgFy: O melhor streaming de música---")
+    print("---Cadastro de músicas---")
 
-while True:
-    ##Inicio do item a)
+    while True:
+        ##Inicio do item a)
 
-    ##Fim do item b)
-    print("---Menu principal---")
-    print("1 - Tocar música")
-    print("2 - Calcular audiência")
-    print("3 - Qual é a música?")
-    print("0 - sair.")
+        ##Fim do item b)
+        print("---Menu principal---")
+        print("1 - Tocar música")
+        print("2 - Calcular audiência")
+        print("3 - Qual é a música?")
+        print("0 - sair.")
 
-    while opcao != 0:
-        opcao = int(input("Digite a opção desejada:>"))
-        if opcao == 1:
-            pass
-            ##inicio do item b)
+        while opcao != 0:
+            opcao = int(input("Digite a opção desejada:>"))
+            if opcao == 1:
+                pass
+                ##inicio do item b)
 
-            ##Fim do item b)
-        elif opcao == 2:
-            pass
-            ##Inicio do item c)
+                ##Fim do item b)
+            elif opcao == 2:
+                pass
+                ##Inicio do item c)
 
-            ##fim do item c)
-        elif opcao == 3:
-            pass
-            ##Inicio do item d)
+                ##fim do item c)
+            elif opcao == 3:
+                pass
+                ##Inicio do item d)
 
-            ##Fim do item d)
-        elif opcao == 0:
-            print("Encerando a execução...")
-        else:
-            print("Opção incorreta. Opções disponiveis")
-            print("1 - Tocar música")
-            print("2 - Calcular audiência")
-            print("3 - Qual é a música?")
-            print("0 - sair.")
+                ##Fim do item d)
+            elif opcao == 0:
+                print("Encerando a execução...")
+            else:
+                print("Opção incorreta. Opções disponiveis")
+                print("1 - Tocar música")
+                print("2 - Calcular audiência")
+                print("3 - Qual é a música?")
+                print("0 - sair.")
