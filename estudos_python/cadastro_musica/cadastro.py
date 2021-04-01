047658b72c843e9c82a0af8c961a8dfdff2e424e
roberto <robertoweller78@gmail.com>
2021-04-01 04:00:25 -0300

    Já calcula a media do ibope

--- estudos_python/cadastro_musica/cadastro.py
+++ estudos_python/cadastro_musica/cadastro.py
@@ -23,6 +23,9 @@ while True:
 
     """
     ##Inicio do item a)
+
+    tocadas = {}
+
     # Perguntas do cadastros para o usuário
     nome_musica = input("Dígite o nome da música: ")
     nomes.append(nome_musica)
@@ -38,7 +41,7 @@ while True:
     ] = f"Música tocada. Nome: {nome_musica}. Cantor: {nome_cantor}. Letra: {letra_cantor}"
 
     # Confirma se tem mais cadastros
-    continua = input("Deseja cadastrar outra música? (1 - sim. Outro - não ): >")
+    continua = input("Deseja cadastrar outra música? (1 - sim. Outro - não ): > ")
 
     # Se não tem mais cadastros à fazer, analize os que já tem
     if continua != "1":
@@ -64,22 +67,54 @@ while True:
                 ##inicio do item b)
                 achar_nome = input("Dígite o nome de uma música: > ")
 
-                # Mostra para o usuário a música já com todas as informações
-                print(letras[achar_nome])
+                # Se a mpusica estive cadastrada, exiba ela
+                if achar_nome in letras:
+
+                    # Mostra para o usuário a música já com todas as informações
+                    print(letras[achar_nome])
+
+                    # Se o nome estiver já nas tocadas
+                    if achar_nome in tocadas:
+
+                        tocadas[achar_nome] += 1
+
+                    else:
+
+                        tocadas[achar_nome] = 1
+
+                # Não estando cadastrado, volte ao menu principal
+                else:
+                    print("Essa música não está cadastrada.")
 
                 ##Fim do item b)
             elif opcao == 2:
-                pass
+                """
+                -----    Exemplo de saída no terminal:    -----
+
+                Nome: Nome1. Audiência de: 50.00 %
+                Nome: Nome2. Audiência de: 50.00 %
+
+                -----  Aqui vai o ocorrer o cálculo da audiência  -----
+                """
                 ##Inicio do item c)
+
+                # Extraindo todas músicas tocadas e quantidade que foram
+                for musica, quantidade in tocadas.items():
+                    # Tira a pocentagem de vezes tocadas
+                    ibope = (quantidade * 100) / len(tocadas)
+
+                    print(f"{musica}. Audiência de: {ibope} %.")
 
                 ##fim do item c)
             elif opcao == 3:
+
+                # Vai procurar a música
+
                 pass
                 ##Inicio do item d)
 
                 ##Fim do item d)
             elif opcao == 0:
-                print("Encerando a execução...")
                 quit()
 
             else:
