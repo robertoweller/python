db40cd1b944069de3a66805df386ebab9b5dccfc
roberto <robertoweller78@gmail.com>
2021-04-02 01:39:18 -0300

    tentar detectar palavras dentro de listas

---
+++ estudos_python/cadastro_musica/tes.py
@@ -0,0 +1,20 @@
+trecho = "vez"
+posicao = -1
+
+conta = 0
+
+achou = ""
+
+td_letras = ["era vez", "estrada vida"]
+td_letras = ["Zé da gaita", "João Bosco"]
+
+
+i = trecho
+
+for cc in td_letras:
+
+    if trecho in cc:
+        print(cc)
+        # posicao = conta
+
+# print(td_letras[posicao])
