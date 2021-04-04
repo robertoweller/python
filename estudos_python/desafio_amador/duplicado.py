08ad0b3b2b3c17578670801b79c7d9cf7423cbda
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-04 19:20:54 -0300

    Acha o numero duplicado

---
+++ estudos_python/desafio_amador/duplicado.py
@@ -0,0 +1,24 @@
+"""
+Encontre o número duplicado
+
+Dado um array nums contendo n + 1 inteiros onde cada inteiro está entre 
+1 e n (inclusive), prove que pelo menos um número duplicado deve existir. 
+Suponha que haja apenas um número duplicado, encontre o número duplicado.
+
+Exemplo 1:
+
+Entrada: [1,3,4,2,2]
+Produto: 2
+
+
+Exemplo 2:
+
+Entrada: [3,1,3,4,2]
+Produto: 3
+Observação:
+
+Você não deve modificar a matriz (suponha que a matriz seja somente leitura).
+Você deve usar apenas espaço extra constante O (1).
+Sua complexidade de tempo de execução deve ser menor que O (n2).
+Há apenas um número duplicado na matriz, mas pode ser repetido mais de uma vez.
+"""
