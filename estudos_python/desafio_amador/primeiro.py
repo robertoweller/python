a714c2a69615490dfdc3eed1f30f8e1fa36302cb
roberto <robertoweller78@gmail.com>
2021-04-04 03:12:05 -0300

    Primeira solução para as somas

--- estudos_python/desafio_amador/primeiro.py
+++ estudos_python/desafio_amador/primeiro.py
@@ -1,13 +1,27 @@
-class Principal:
-    def roda(self, convers):
+from time import sleep
 
-        return convers
+# Tentado enteder porque leva tempo para processar essa criptografia
+class Solution:
+    def twoSum(self, nums, target):
+
+        for i in range(0, len(nums)):
+            sleep(1)
+            print(f"{i=}")
+
+            for j in range(i + 1, len(nums)):
+
+                print(f"{j=}")
+
+                if nums[i] + nums[j] == target:
+
+                    return [i, j]
+
+        return None
 
 
-cnvs = ["conversas"]
+nums = [11, 15, 2, 7]
+target = 9
 
-# Leitor de documento de texto.
-leitor = Principal().roda(cnvs)
+exibi = Solution().twoSum(nums, target)
 
-
-print(leitor)
+print(exibi)
