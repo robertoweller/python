3601d206f3cd60c3f41c32f9f18c41e4631fc0b6
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-05 19:20:27 -0300

    Resolução foi tirar tirar duas das linhas

---
+++ estudos_python/alt-larg.py
@@ -0,0 +1,29 @@
+def primeiro():
+    largura = int(input("Digite a largura: "))
+
+    altura = int(input("Digite a altura: "))
+    caracter = str(input("Digite o carácter: "))
+    linha = -1
+
+    # Esse está fazenod impressão da altura
+    while linha <= altura:
+
+        print(caracter, end="")
+        coluna = 2
+
+        # Está fazendo a impressão da largura
+        while coluna < largura:
+
+            if linha == -1 or linha == altura or coluna == largura:
+                print(caracter, end="")
+            else:
+                print(end=" ")
+
+            coluna = coluna + 1
+
+        linha = linha + 1
+
+        print(caracter)
+
+
+primeiro()
