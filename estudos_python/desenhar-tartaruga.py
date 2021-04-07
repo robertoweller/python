b82e3e9d1d354791478f2aa319e8932d342d9668
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-06 23:24:16 -0300

    Resolvido passar argumento sem chamar a função.

--- estudos_python/desenhar-tartaruga.py
+++ estudos_python/desenhar-tartaruga.py
@@ -1,11 +1,22 @@
+from functools import partial
+
+
 def roda():
     """
     Eu quero passar a string, mas não quero chamar a função escreve
     """
-    escreve("String como argumento!")
-
+    letras = ["c", "a", "d", "f", "b", "e"]
+    procura = 0
+    for achou in letras:
+        if achou == "d":
+            # Digamos que você precise passar agora, mas não que chamar a função
+            passando = partial(escreve, f"Achou! estava na posição {procura} da lista!")
+        procura += 1
     # Porque a função escreve já esta sendo chamado por outra chamada
-    escreve()
+    # escreve()
+
+    # E só agora você que chamar a função, mas não que passar argumento
+    passando()
 
 
 def escreve(arg):
