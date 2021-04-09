47370ef52c756feca60ab2a7e00f3e4038afa93e
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-09 17:47:03 -0300

    Aula de exibir data nascimento para iniciante

---
+++ estudos_python/curso_python/data-nasci.py
@@ -0,0 +1,13 @@
+"""
+Crie um script Python qye leia o dia
+o mês e o ano de nascimento
+de uma pessoa e mostre uma 
+mensagem com data formatada.
+
+"""
+
+dia = input("Dia = ")
+mes = input("Mês = ").title()
+ano = input("Ano = ")
+
+print(f"Você nasceu no dia {dia} de {mes} de {ano}. Correto?")
