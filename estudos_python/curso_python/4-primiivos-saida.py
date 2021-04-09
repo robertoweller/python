903a2ff734a0464c512ca3a8ac4440018de83dc3
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-09 19:18:08 -0300

    Documentaçao do exercicio

---
+++ estudos_python/curso_python/4-primiivos-saida.py
@@ -0,0 +1,19 @@
+"""
+Faça um programa que leia algo pelo
+teclado e mostre na tela o seu
+tipo primitivo e todas as informações
+possiveis sobre ele.
+
+Exemplo de saida:
+----------------------------------------------
+Digite algo: Programador: Programador
+O tipo promitivo desse valor é <clas 'str'>
+Só tem espaços?  False
+É um número?  False
+É alfabético?  True  
+Está em maiúsculas?  False
+Está em minúsculas?  False
+Está capitalizada?  True
+
+
+"""
