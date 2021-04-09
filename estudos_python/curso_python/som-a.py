2ef5ba8c28e1beca6fefde6f6a68a74d4b59e240
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-09 18:14:15 -0300

    Outro modo de resolver jgando a soma para uma variavel.

---
+++ estudos_python/curso_python/som-a.py
@@ -0,0 +1,11 @@
+"""
+Outro modo de resolver a soma 
+do exercício.
+
+"""
+
+n1 = input("Digite um número: ")
+n2 = input("Digite outro número: ")
+s = int(n1) + int(n2)
+
+print(f"A soma dos número é {s}.")
