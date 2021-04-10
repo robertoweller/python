06a0c5ec7f075d6b34267c57277ac277b4dab770
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-10 20:04:17 -0300

    Codigo alternativo, modo mais simples de resolver

---
+++ estudos_python/validar_produto/alternativo.py
@@ -0,0 +1,77 @@
+"""
+    Escreva um programa que receba com parâmetro
+de entrada um número inteiro de 5 dígito no 
+intervalo fechado [10000, 3000] que represente 
+código de produtos vendidos em uma loja. Crie uma
+funçao para validar os dados de entrada, obrigando
+o usuário a respeitar o intervalo e o tipo de 
+dado (inteiro).
+    Crie uma função que calcule e retorne o dígito
+veridicador do código, utilizando a regra de 
+cáculo explicada a seguir. Por exemplo, considere
+o código 21853, em que cada dígito é multiplicado
+por um peso começando em 2, os valores obtidos são,
+e do total obtido calcula-se o resto de sua divisão 
+por 7.
+--------------------------------------------------------------------------------
+    Dígito     |   2   |   1   |   8    |   5   |   3   |
+     Peso      |   2   |   3   |   4    |   5   |   6   |
+ Multiplicação |   4   |   3   |   32   |   25  |   18  | Soma total = 82
+               |       |       |        |       |       | Resto de 82 por 7 = 5
+--------------------------------------------------------------------------------
+    Retorne na função o valor do dígito verificador
+calculado e imprima na tela o código do produto dígito
+verificador sepado por hífen, como: 21853-5.
+"""
+
+
+def validated(e):
+    # Se for número.
+    if e.isnumeric():
+
+        if int(e) >= 10000 and int(e) <= 30000:
+
+            # Os prints são opcionais, porque o exercicio não pede
+            # print("Valor está dentro do aceitavel")
+
+            return True
+
+        else:
+
+            # print("Por favor digite valor entre 1000 e 3000")
+
+            return False
+
+    else:
+        # print("Por favor digite apenas números. E tente novamente")
+
+        return False
+
+
+def main(code=21853):
+
+    # Deixando como o exercicio pede
+    code = str(code)
+    total = 0
+    acrescenta = 2
+    # Entrada do usuario também é opcional, porque o exercicio não pede
+    # codigo = input("Digite o código: ")
+
+    # Se validou.
+    if validated(code):
+        for i in code:
+            # Vai multiplicar cada um dos caracteres
+            # e acrescentar ao valor total
+            # Jeito mais simples de resolver
+            total += int(i) * acrescenta
+            acrescenta += 1
+
+        # código do produto
+        p = f"{code}-{total % 7}"
+        print(p)
+
+        return total
+
+
+if __name__ == "__main__":
+    main()
