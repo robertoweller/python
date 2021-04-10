c6c47d43bb45bc2a6d83e16e93eaa73ef3d443a5
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-10 19:37:55 -0300

    Codigo pronto e corrigido, deixei em ingles para facilitar

--- estudos_python/validar_produto/main.py
+++ estudos_python/validar_produto/main.py
@@ -6,7 +6,6 @@ código de produtos vendidos em uma loja. Crie uma
 funçao para validar os dados de entrada, obrigando
 o usuário a respeitar o intervalo e o tipo de 
 dado (inteiro).
-
     Crie uma função que calcule e retorne o dígito
 veridicador do código, utilizando a regra de 
 cáculo explicada a seguir. Por exemplo, considere
@@ -14,20 +13,15 @@ o código 21853, em que cada dígito é multiplicado
 por um peso começando em 2, os valores obtidos são,
 e do total obtido calcula-se o resto de sua divisão 
 por 7.
-
-
 --------------------------------------------------------------------------------
     Dígito     |   2   |   1   |   8    |   5   |   3   |
      Peso      |   2   |   3   |   4    |   5   |   6   |
  Multiplicação |   4   |   3   |   32   |   25  |   18  | Soma total = 82
                |       |       |        |       |       | Resto de 82 por 7 = 5
 --------------------------------------------------------------------------------
-
     Retorne na função o valor do dígito verificador
 calculado e imprima na tela o código do produto dígito
 verificador sepado por hífen, como: 21853-5.
-
-
 """
 
 
