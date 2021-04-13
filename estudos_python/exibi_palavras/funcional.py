c148a1faeb9fe1201f3f2e8c7180b795382eb01b
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-13 09:53:32 -0300

    Correcao do bug, agora int retorna zero se for passado im string vazia ou com algo

--- estudos_python/exibi_palavras/funcional.py
+++ estudos_python/exibi_palavras/funcional.py
@@ -2,13 +2,13 @@ def int_(a):
     a = str(a)
 
     if a.isdecimal():
-
         return int(a)
     if a == "":
         return 3
+    return 0
 
 
-def for_(tds, re=3):
+def for_(tds, re):
     # Se que que mais palavras só adicionar a lista
     pp = re * tds
 
@@ -18,7 +18,7 @@ def for_(tds, re=3):
 
 
 palavra = input("Digite uma palavra: ")
-palavra_2 = input("digite outra palavra: ")
+palavra_2 = input("Digite outra palavra: ")
 rep = int_(input("Digite o número de repetições: "))
 # # Se que que mais palavras só adicionar a lista
 tds = [palavra, palavra_2]
