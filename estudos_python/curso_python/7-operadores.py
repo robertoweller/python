4d48d077e24f1c512d0e62725b097f5abd5dce96
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-19 08:51:06 -0300

    Exercicio feito.

--- estudos_python/curso_python/7-operadores.py
+++ estudos_python/curso_python/7-operadores.py
@@ -2,44 +2,12 @@
 
 
 Desafio 007
-Desenvolva um programa qu eleia as duas
+Desenvolva um programa que eleia as duas
 notas de um aluno, calcule e mostre a média.
 
-
-Desafio 008
-Escreva um programa que leia um valor em metros e mostre
-e exiba convertido em centimetro e milimitros.
-
-
-Desafio 009
-Faça um programa que leia um número inteiro qualquer e mostre
-na tela a sua tabuada.
-
-
-Desafio 010
-Crie um programa que leia quanto dinheiro uma pessoa tem na carteira
-e mostre quantos Dólares ela pode comprar.
-
-Considere
-US$1.00 = 3.27
-
-
-Desafio 011
-Faça um programa que leia a largura e altura de
-uma parede e mostre, calcule a sua área e a 
-quantidade de tinta necessária para pintá-la,
-sabendo que cada litro de tinta, pinta um área de 2m².
-
-
-Desafio 012
-Faça um algoritimo que leia o preço de um produto
-e mostre seu novo preço com 5% de desconto.
-
-
-Desafio 013
-Faça um algoritmo que leia o salário de um 
-funcionário e mostre seu novo salário, com 15% de aumento.
-
-
-
 """
+
+nota1 = int(input('Nota 1: '))
+nota2 = int(input('Nota 2: ' ))
+
+print(f'A média é {(nota1 + nota2) / 2}')
