4b1283bb0f93ea474642a5978bf28b5d749e30c1
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-19 07:16:25 -0300

    Pandan se mostrando otimo para lidar com bancos de dados

--- Pandas/1-test.py
+++ Pandas/1-test.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import json
 
 # Dois exemplos de bancos de dados
+# Exemplo 1, criando aqui mesmo o banco
 df = pd.DataFrame(     
   {
    "Name": [
@@ -14,7 +14,20 @@ df = pd.DataFrame(
   }
  )
 
-# Carregar o json
-js = json.load("verifica_extensoes/Programming_Languages_Extensions.json")
 
-print(js)
+# Exemplo 2 abrindo um externo, Pandas aceita diversos tipo até json
+# Agora simplificando o banco de dados com Pandas
+simple = pd.read_json("verifica_extensoes/Programming_Languages_Extensions.json")
+
+# Exibindo o banco simplificado num iterrows
+for a, b in simple.iterrows():
+    # print('--------------------------------\n', a,)
+    if '.pas' in b['extensions']:
+
+        print(b, end="\n\n")
+
+
+# Outro modo de abrir json
+# Carregar o json
+# abre = open("verifica_extensoes/Programming_Languages_Extensions.json", "r")
+# js = json.load(abre)
