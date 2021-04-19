a08298b85f521af68c869ee9244e2056517e7568
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-19 09:35:16 -0300

    Falta resolver!

---
+++ estudos_python/curso_python/12-operadores.py
@@ -0,0 +1,18 @@
+"""
+Desafio 012
+Faça um algoritimo que leia o preço de um produto
+e mostre seu novo preço com 5% de desconto.
+"""
+
+
+
+
+
+
+
+"""
+Desafio 013
+Faça um algoritmo que leia o salário de um 
+funcionário e mostre seu novo salário, com 15% de aumento.
+
+"""
