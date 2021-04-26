4986e7388faf1e7e2cb35eac53800d453ca351ee
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-26 05:06:15 -0300

    Falta resolver

---
+++ estudos_python/sla.py
@@ -0,0 +1,15 @@
+a = [["a", 50], ["b", 60], ["c", 70]]
+
+
+"""
+for gg, ff in a:
+
+    print(ff)
+
+"""
+
+sla = [["a", 60], ["b", 72], ["c", "nao é numero"]]
+
+for a, b in sla:
+
+    print(b[1])
