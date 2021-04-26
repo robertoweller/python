f789284813eea32f8d131adea29c0f19abe7fb57
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-04-26 05:00:16 -0300

    resolvido

--- estudos_python/curso_python/12-operadores.py
+++ estudos_python/curso_python/12-operadores.py
@@ -4,8 +4,17 @@ Faça um algoritimo que leia o preço de um produto
 e mostre seu novo preço com 5% de desconto.
 """
 
+# v = [[chr(i), f'%{i-24}'] for i in range(0, 200)]
 
+v = chr(50)
+# 41 A %41
 
+# 65 A
+
+#
+print("A \23b")
+
+# print(65-24)
 
 
 
