63d01f8d3efcdabc9239efeb0a4dcf15297beb9f
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-05-01 12:20:10 -0300

    if

--- class_objs/sla_class.py
+++ class_objs/sla_class.py
@@ -2,18 +2,24 @@ sla = True
 
 if sla:
     class PessoaFisica:
-        def __init__(self):
-            self.cpf = '000.000.0...-00'
-            self.rg = '...'
-            # etc..
+        def __init__(self, cpf, rg):
+            self.cpf = cpf
+            self.rg = rg
+            # etc...
 
     class PessoaJuridica:
-        def __init__(self):
+        def __init__(self, cnpj, rgi):
             self.cnpj = '...'
             self.registroImpresa = '...'
-            # etc
+            # etc...
 
     print("sim")
+    PessoaFisica('45', '69')
 
 else:
+    # Note que se sla for False vai dar um erro pq aqui não foi declarado as
+    # classes
+    # NameError: name 'PessoaFisica' is not defined
+    # pq pessoa depois de else não foi definida só foi feito depois de if
+    PessoaFisica('45', '69')
     print("não")
