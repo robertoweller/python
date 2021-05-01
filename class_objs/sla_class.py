e1d317df7cfcbf099888783cd79525697a525812
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-05-01 11:54:01 -0300

    if

---
+++ class_objs/sla_class.py
@@ -0,0 +1,18 @@
+
+
+
+if __name__ == '__main__':
+    class Sla():
+        pass
+
+
+    class Kkk():
+        pass
+
+    
+    sla = Sla().__class__.__name__
+    
+    kkk = Kkk().__class__.__name__
+
+    if kkk == 'Kkk':
+        print('Kkk é uma classe kkkkk')
