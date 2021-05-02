abb6c231ae76e0d184a8737ea330526db09337c4
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-05-02 09:29:30 -0300

    testando

---
+++ class_objs/teste.py
@@ -0,0 +1,19 @@
+
+
+class Brincar:
+    def __init__(self, n):
+        self.esc = n
+
+    def escreve(self):
+        print(self.esc)
+
+
+texto = '41'
+
+texto  = Brincar(texto)
+
+texto.escreve()
+
+
+
+
