8d3c5e0c217c12c5a3c8150c8caca0fa6296e2d7
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-05-11 19:57:26 -0300

    Melhorado!

--- estudos_python/curso_python/12-13operadores.py
+++ estudos_python/curso_python/12-13operadores.py
@@ -4,11 +4,11 @@ Faça um algoritimo que leia o preço de um produto
 e mostre seu novo preço com 5% de desconto.
 """
 
-preco = input('Digite o preço do produto: ')
+preco = float(input('Qual o preço do produto? R$'))
 # Com desconto de 5%
-produto = int(preco)-(int(preco)*5)/100
+produto = preco * 0.95
 
-print(f'Produto com desconto de 5% ${produto}')
+print(f'Produto que custava R${preco:.2f}, na promoção com desconto 5% vai custar R${produto:.2f}')
 
 """
 Desafio 013
@@ -17,8 +17,8 @@ funcionário e mostre seu novo salário, com 15% de aumento.
 
 """
 
-salario  = input("Digite seu salário: ")
+salario  = float(input("Digite seu salário: R$"))
 # Salário com 15% de almento
-almento = int(salario) + (int(salario)*15)/100
-
-print(f'Seu saláro com almento de 15% ${almento}')
+# almento = salario + (salario*15)/100
+almento = salario * 1.15
+print(f'Seu saláro de {salario:.2f}, com almento de 15% vai para R${almento:.2f}')
