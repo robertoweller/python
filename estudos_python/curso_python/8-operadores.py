4f7f60ae4cec093ac56bf6c8d22098611ed1dbb2
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-05-11 17:19:18 -0300

    Melhorado!

--- estudos_python/curso_python/8-operadores.py
+++ estudos_python/curso_python/8-operadores.py
@@ -3,9 +3,20 @@ Desafio 008
 Escreva um programa que leia um valor em metros e mostre
 e exiba convertido em centimetro e milimitros.
 """
-metros = int(input("Digite o valor em mentros: "))
+metros = float(input("Digite o valor em mentros: "))
 
+km = metros/1000
+hm = metros/100
+dam = metros/10
+dm = metros*10
 centimitros = metros*10**2
 milimitros = metros*10**3
 
-print(f'{centimitros=}\n{milimitros=}')
+print(
+    f'A medida de {metros:.1f}m corresponde a\n'
+    f'{km}km\n'
+    f'{hm}hm\n'
+    f'{dam}dam\n'
+    f'{dm}dm\n'
+    f'{centimitros}cm\n'
+    f'{milimitros}mm')
