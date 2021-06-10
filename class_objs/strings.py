277cde4ff8cb40f5cc2105810a9f456255c5d2a5
Roberto Ribeiro Weller <robertoweller78@gmail.com>
2021-06-10 20:27:33 -0300

    Treino

---
+++ class_objs/strings.py
@@ -0,0 +1,8 @@
+
+# Número teste
+n = 123456
+# Retorna o número revertido de n de main
+main = lambda n:  ''.join(map(str, list(reversed(list(str(n))))))
+# Monstrando o valor revertido
+print(main(n))
+
